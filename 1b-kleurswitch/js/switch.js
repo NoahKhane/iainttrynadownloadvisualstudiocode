198d28c1c5c27d9976612df2c3de42a4fe164646
Noah Khan <102799@glr.nl>
2025-01-07 11:02:17 +0100

    Yuurrr

---
+++ 1b-kleurswitch/js/switch.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const tekst = document.getElementById('tekst');
+
+button.addEventListener('click', function() {
+    tekst.style.color = 'green';
+});
